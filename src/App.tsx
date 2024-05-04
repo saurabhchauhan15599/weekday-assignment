76cc83a1071998abf4f4a3c3a32479fffe438704
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2024-05-05 01:51:35 +0530

    infinite-scroll implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useRef } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
   const queryClient = new QueryClient();
+  const rootContainer = useRef<HTMLElement>(null);
+
   return (
-    <>
+    <main ref={rootContainer}>
       <QueryClientProvider client={queryClient}>
-        <Card />
+        <Card ref={rootContainer} />
       </QueryClientProvider>
-    </>
+    </main>
   );
 }
 
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,42 +1,88 @@
-import { useMutation } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
-import { IAPIData, IData } from "../../helpers/types";
-import { payloadObject } from "../../helpers/utils";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { RefObject, useState } from "react";
+import { IData } from "../../helpers/types";
 import { fetchJobInfo } from "../../service/fetchInfo.service";
+import InfiniteScroll from "../InfiniteScroller";
+import styles from "./Card.module.scss";
 import CardBody from "./CardBody";
 import CardFooter from "./CardFooter";
 import CardHeader from "./CardHeader";
-import styles from "./Card.module.scss";
 
-const Card: React.FC = () => {
-  const [cardData, setCardData] = useState<IData[]>([]);
+interface IPageOptions {
+  currentPage: number;
+  hasNext: boolean;
+}
 
-  const { mutateAsync } = useMutation({
-    mutationFn: (payload: typeof payloadObject) => {
-      return fetchJobInfo(payload ?? payloadObject);
-    },
-    onSuccess: (data: IAPIData) => {
-      console.log(data);
-      setCardData(data.jdList);
-    },
-    onError: (error) => {
-      console.error(error);
-    },
+interface ICard {
+  ref: RefObject<HTMLElement>;
+}
+
+const Card: React.FC<ICard> = ({ ref }) => {
+  const [pageOptions, setPageOptions] = useState<IPageOptions>({
+    currentPage: 1,
+    hasNext: true,
   });
+  const { currentPage, hasNext } = pageOptions;
+  const fetchData = async ({
+    pageParam,
+  }: {
+    pageParam: number;
+  }): Promise<{
+    data: IData[];
+    currentPage: number;
+    nextPage: number;
+  }> => {
+    const payload = { offset: pageParam, limit: 10 };
+    const response = await fetchJobInfo(JSON.stringify(payload));
+    setPageOptions((prev) => ({
+      ...prev,
+      currentPage: pageParam,
+      hasNext: response.totalCount > 0,
+    }));
+    return {
+      data: response.jdList ?? [],
+      currentPage: pageParam,
+      nextPage: pageParam + 1,
+    };
+  };
 
-  useEffect(() => {
-    mutateAsync(payloadObject);
-  }, []);
+  const { data, error, fetchNextPage, hasNextPage, isFetching } =
+    useInfiniteQuery({
+      queryKey: ["jobPosts"],
+      queryFn: fetchData,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+      refetchOnWindowFocus: false,
+    });
 
   return (
     <main className={styles.cardContainer}>
-      {cardData?.map((values) => (
-        <div key={`card-${values.jdUid}`} className={styles.card}>
-          <CardHeader data={values} />
-          <CardBody data={values} />
-          <CardFooter data={values} />
-        </div>
-      ))}
+      <InfiniteScroll
+        rootRef={ref?.current!}
+        hasMore={hasNextPage || !isFetching}
+        currentIndex={currentPage}
+        onIntersect={() => fetchNextPage()}
+      >
+        {!error ? (
+          data?.pages.map((page, i) => {
+            return (
+              <React.Fragment key={i}>
+                {page.data.map((item) => {
+                  return (
+                    <div key={`card-${item.jdUid}`} className={styles.card}>
+                      <CardHeader data={item} />
+                      <CardBody data={item} />
+                      <CardFooter data={item} />
+                    </div>
+                  );
+                })}
+              </React.Fragment>
+            );
+          })
+        ) : (
+          <p>{error.message}</p>
+        )}
+      </InfiniteScroll>
     </main>
   );
 };
