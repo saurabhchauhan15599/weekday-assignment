7f5b726dc04fe824ea7059973aa99c41d0924d5a
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2024-05-05 03:21:54 +0530

    filters added

---
+++ src/components/Card/constant.ts
@@ -0,0 +1,17 @@
+export const EXP_MAP = [
+  { value: "", label: "Select exp" },
+  { value: 1, label: 1 },
+  { value: 2, label: 2 },
+  { value: 3, label: 3 },
+  { value: 4, label: 4 },
+  { value: 5, label: 5 },
+]
+
+export const ROLE_MAP = [
+  { value: '', label: 'Select Role' },
+  { value: 'frontend', label: 'Frontend' },
+  { value: 'tech lead', label: 'Tech Lead' },
+  { value: 'backend', label: 'Backend' },
+  { value: 'ios', label: 'IOS' },
+  { value: 'android', label: 'Andriod' },
+]
